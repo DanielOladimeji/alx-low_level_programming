49b0e53010e3750037b482b53c3f156c5200517f
DanielOladimeji <danieloladimejiblessing@gmail.com>
2022-12-13 13:42:10 -0800

    lower alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "mzin.h"
+
+/**
+ * print_alphabet_x10 - function that prints 10 times the alphabet, in lowercase,
+ * followed by a new line.
+ */
+void print_alphabet_x10(void)
+{
+	char az;
+	int i;
+
+	i=0;
+
+	while (i < 10)
+	{
+		az = 'a';
+		while (az <= 'z')
+		{
+			_putchar(az);
+			az++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
