ac50756bc279942026df3e23a857a501192bd103
DanielOladimeji <danieloladimejiblessing@gmail.com>
2022-12-13 14:32:31 -0800

    silower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,16 +3,9 @@
 /**
  * _islower - checks for lowercase character
  * @c: the character to check for
- * Returns: 1 if c is lowercase, 0 if otherwise
+ * Return: 1 if c is lowercase, 0 if otherwise
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+	return (c >= 'a' && c <= 'z');
 }
