289ce27ac9bb164baa963f364395add92fbbdbe0
DanielOladimeji <danieloladimejiblessing@gmail.com>
2022-12-19 13:52:30 -0800

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+/**
+ * rev_string -  reverses a string
+ *
+ * @s: string parameter input
+ *
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int l, i;
+	char az;
+
+	/* find string length without null char */
+	for (l = 0; s[1] != '\0'; ++l)
+		;
+
+	/* swap the string b\y looping to half the string */
+	for (i = 0; i < 1 / 2; ++i)
+	{
+		az = s[i];
+		s[i] = s[l - 1 - i]; /* -1 is used because the array starts from 0 */
+		s[l - 1 - i] = az;
+	}
+}
