7dbf179a880b907b79bab04a136fbcb30707136f
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-01-09 09:32:41 -0800

    Malloc free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,7 @@ char *str_concat(char *s1, char *s2)
 
 	while (s1 && s1[len1])
 		len1++;
-	while (s2 && s2[len2])
+	while (s2 && s1[len2])
 		len2++;
 
 	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
@@ -34,16 +34,17 @@ char *str_concat(char *s1, char *s2)
 			i++
 		}
 	}
+
 	if (s2)
 	{
-		while (i < (len1 + len2))
+		while (i < len1 + len2))
 		{
 			s3[i] = s2[j];
 			i++;
 			j++;
 		}
 	}
-	s3[i] = '\0';
+		s3[i] = '\0';
 
-	return (s3);
+		return (s3);
 }
