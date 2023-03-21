ceaaa0746d5f67a5111c3759aa417416b9a1d9b4
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-03-20 17:34:21 -0700

    task 1n2

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper -  checks for uppercase character
+ * @c: character to br checked
+ * Return: 1 if c is uppercase, 0 if otherwise
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
