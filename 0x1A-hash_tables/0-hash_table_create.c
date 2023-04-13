653f43e6a62ce787cd75ccff1aeb15831a5a846d
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-04-12 21:30:27 -0700

    HT try

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,9 +3,8 @@
 /**
  * hash_table_create - creates a hash table
  * @size: size of the array
- *
- * Return: returns pointer to the newly generated hash table
- * if fails, returns NULL
+ * Return: returns pointer to the newly created hash table
+ * failure, returns NULL
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
