0cd7c3e6d6ba7817f32ef6bdba37de5494ca348e
DanielOladimeji <Danieloladimejiblessing@gmail.com>
2023-04-12 21:10:05 -0700

    Hash Table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,16 +14,18 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int i;
 
-	table = malloc(sizeof(*table));
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
-	table->array = malloc(sizeof(*table->array) * size);
+	table->array = calloc(table->size, sizeof(hash_node_t *));
 	if (table->array == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
+	for (i = 0; i < table->size; i++)
+	{
 		table->array[i] = NULL;
+	}
 
 	return (table);
 }
